4c0b7c7191c7b94e1506f5a15c7def75b6187b17
Jaime Escobar Martinez <31583280+KrazyJames@users.noreply.github.com>
2023-09-24 00:05:10 -0700

    Update NoNetworkView.swift

--- MarvelApp/Views/Utils/NoNetworkView.swift
+++ MarvelApp/Views/Utils/NoNetworkView.swift
@@ -9,22 +9,24 @@ import SwiftUI
 
 struct NoNetworkView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(colors: [.black, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
-            VStack(spacing: 40) {
-                SwiftUI.Image(systemName: "wifi.slash")
-                    .font(.system(size: 100))
-                Text("No network connection ☹️")
-                    .font(.title3)
-                    .bold()
-            }
-        }
+        ContentUnavailableView(
+            "Internet is not reachable",
+            systemImage: "wifi.slash"
+        )
+        .background(
+            LinearGradient(
+                colors: [
+                    .black,
+                    .blue
+                ],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+        )
+        .ignoresSafeArea()
     }
 }
 
-struct NoNetworkView_Previews: PreviewProvider {
-    static var previews: some View {
-        NoNetworkView()
-            .preferredColorScheme(.dark)
-    }
+#Preview {
+    NoNetworkView()
 }
